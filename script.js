2d6442c9a79cf6d1152395a7e1b45a898c1c0ee9
nielmontesa <nirumontesa@gmail.com>
2024-08-26 15:51:27 +0800

    First version!

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const textarea = document.querySelector("textarea");
+    const playerContainer = document.querySelector(".player-container");
+    const statusElement = document.querySelector(".status");
+
+    textarea.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevents the default action of the "Enter" key
+            const cssCode = textarea.value.trim();
+            
+            if (cssCode === "justify-content: flex-end;") {
+                playerContainer.style.cssText += cssCode;
+
+                // Update status element
+                statusElement.textContent = "Correct! Go to next level.";
+                statusElement.style.color = "#82ff4d";
+
+                // Play the "win" sound
+                new Audio("public/win.mp3").play();
+            } else {
+                playerContainer.style.cssText += cssCode;
+                // Update status element
+                statusElement.textContent = "Wrong! Try again.";
+                statusElement.style.color = "#ff4d53";
+
+                // Play the "wrong" sound
+                new Audio("public/wrong.mp3").play();
+            }
+        }
+    });
+});
